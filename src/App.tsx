7160fc2fa94550edce6ba14acc9ffb372cbfa264
valfink <valentin.finke@gmail.com>
2023-02-14 16:27:33 +0100

    Add stuff

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,6 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Hello {greetingName}</h1>
-        <input type="text" id="nameInput" /><button onClick={() => setGreetingName(document.getElementById("nameInput").value)}>Set Greeting</button>
       </header>
     </div>
   );
