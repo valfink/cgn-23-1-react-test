239eb41a3a9d5db05820df346029ce5be18ab256
valfink <valentin.finke@gmail.com>
2023-02-14 16:27:06 +0100

    Add stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [greetingName, setGreetingName] = useState("Everybody!")
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Hello {greetingName}</h1>
+        <input type="text" id="nameInput" /><button onClick={() => setGreetingName(document.getElementById("nameInput").value)}>Set Greeting</button>
       </header>
     </div>
   );
